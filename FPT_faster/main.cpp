4f4a4bafe3ea20f08d652f06f895d4d6d1f56567
彭云 <pengyun.py@alibaba-inc.com>
2015-01-18 11:57:06 +0800

    Initial Commit

---
+++ FPT_faster/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FPT_faster
+//
+//  Created by Yun Peng on 1/18/15.
+//  Copyright (c) 2015 Yun Peng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
